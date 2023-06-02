25d97b15b2d8d9a5dae9fc528dafe845a4215b38
CristianA <cris4599al@gmail.com>
2023-06-03 01:00:39 +0300

    Auth prod URL

--- WebSinglePageAngularApp/src/environments/environment.prod.ts
+++ WebSinglePageAngularApp/src/environments/environment.prod.ts
@@ -1,5 +1,23 @@
 export const environment = {
-  production: true,
-  apiUrl: 'api/',
-  hubUrl: 'hubs/'
+  production: false,
+
+  authDomain:
+    'authservice.greenflower-4193fb10.northeurope.azurecontainerapps.io',
+  authService:
+    'https://authservice.greenflower-4193fb10.northeurope.azurecontainerapps.io/api/auth/',
+
+  usersDomain: 'localhost:8081',
+  usersService: 'http://localhost:8081/api/users/',
+
+  devicesDomain: 'localhost:8082',
+  devicesService: 'http://localhost:8082/api/devices/',
+
+  notificationsDomain: 'localhost:8083',
+  notificationsService: 'http://localhost:8083/api/notifications/',
+
+  statisticsDomain: 'localhost:8085',
+  statisticsService: 'http://localhost:8085/api/statistics/',
+  hubUrl: 'http://localhost:8082/hubs/',
+  // apiUrl: 'http://192.168.0.104:5000/api/',
+  // hubUrl: 'http://192.168.0.104:5000/hubs/'
 };
