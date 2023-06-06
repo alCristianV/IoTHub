7a4355d973a4a5b929554f65a84950d7210b6ecb
CristianA <cris4599al@gmail.com>
2023-06-06 23:08:31 +0300

    statistics service fix

--- Microservices/StatisticsService/StatisticsService/Repositories/DeviceDataFieldRepository.cs
+++ Microservices/StatisticsService/StatisticsService/Repositories/DeviceDataFieldRepository.cs
@@ -12,6 +12,15 @@ namespace IoTHubAPI.Repositories
     {
         private readonly IMongoCollection<DeviceDataField> _deviceDataFields;
 
+        public DeviceDataFieldRepository(IIotHubDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _deviceDataFields = database.GetCollection<DeviceDataField>(settings.DeviceDataFieldsCollectionName);
+        }
+
         public async Task<DeviceDataField> GetDataField(string dataFieldId) {
             var field = await _deviceDataFields.FindAsync(d => d.Id == dataFieldId);
             return await field.FirstOrDefaultAsync();
